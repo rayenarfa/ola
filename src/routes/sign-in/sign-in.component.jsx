1d5ca74e86458a7fabfb6c38ecec16aec50ddcab
Rayen Arfa <arfarayen20@gmail.com>
2024-12-21 14:40:01 +0100

    fix signin

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,44 +1,24 @@
-<<<<<<< HEAD
 import { signInWithGooglePopup, createUserDocumentFromAuth } from "../../utils/firebase/firebase.utils";
-=======
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils";
->>>>>>> f3f2b62125fdfe862fc4760e78dd340bca2c0129
 
 const SignIn = () => {
     // Create logGoogleUser function
     const logGoogleUser = async () => { 
-<<<<<<< HEAD
-        
-        //const response = await signInWithGooglePopup();
-        
-        //destruct of response and pass in as userAuth 
+        // Destructure the user from the response of signInWithGooglePopup
         const { user } = await signInWithGooglePopup();
         
-        //callig method using response user object
-       const userDocRef= await createUserDocumentFromAuth(user)
-=======
-        const response = await signInWithGooglePopup();
-        console.log(response);  
->>>>>>> f3f2b62125fdfe862fc4760e78dd340bca2c0129
+        // Call the method to create a user document using the user object
+        const userDocRef = await createUserDocumentFromAuth(user);
+        console.log(userDocRef);
     };
 
     return (
         <div>
             <h1>I'm the sign-in page</h1>
             <button onClick={logGoogleUser}>
-<<<<<<< HEAD
-                Sign in with Google Popup
-=======
                 Sign in with Google
->>>>>>> f3f2b62125fdfe862fc4760e78dd340bca2c0129
             </button>
         </div>
     );
 };
 
 export default SignIn;
-<<<<<<< HEAD
-
-
-=======
->>>>>>> f3f2b62125fdfe862fc4760e78dd340bca2c0129
